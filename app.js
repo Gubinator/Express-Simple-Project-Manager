ac18478a9b4ea46d3930fb3dc929e1f06b51eb42
Dominik Gubo <gubo.dominik@gmail.com>
2023-05-23 12:03:27 +0200

    Added functional update and delete routes

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const { error } = require('console');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -32,19 +31,13 @@ app.use(express.json);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/projects.js
+++ routes/projects.js
@@ -14,9 +14,8 @@ const Project = require('../models/projectModel')
 router.post('/add' , async(req, res) => {
 
   var projectData = new Project(req.body);
-  projectData.save().then(item=>{
-    res.render('projects/');
-    res.send("Item saved to the database");
+  projectData.save().then(item=>{ 
+    res.redirect("/projects/all")
   })
   .catch(err => {
     console.log(err.message);
@@ -31,7 +30,7 @@ router.get('/all', async(req, res) => {
     res.format({
       html: function() {
         res.render('projects/all', {
-          title: 'All my Projects',
+          title: 'All projects',
           projects: projects
         });
       },
@@ -62,8 +61,75 @@ router.get('/:id', async(req, res) => {
       }
     });
   } catch (error) {
-    res.setMaxListeners(500).json({message: error.message})
+    res.setMaxListeners(500).json({message: error.message});
   }
 })
 
+router.all('/delete/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findByIdAndDelete(id);
+    if(!project){
+      return res.status(404).json({message: "Cannot find any project with specified ID"})
+    }
+    res.redirect('/projects/all');
+    //res.redirect('/all');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+/*project.update({
+  project_name : name,
+  project_description : badge,
+  project_tasks : dob,
+  project_start_date : isloved
+  project_end_date:*/
+
+router.all('/update/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const filter = { _id : id};
+    var pr_name = req.body.project_name;
+    var pr_description = req.body.project_description;
+    var pr_tasks = req.body.project_tasks;
+    var pr_price = req.body.project_price;
+    var pr_start_date = req.body.project_start_date;
+    var pr_end_date = req.body.project_end_date;
+    
+    const update = { 
+      project_name : pr_name,
+      project_description : pr_description,
+      project_tasks : pr_tasks,
+      project_price : pr_price,
+      project_start_date : pr_start_date,
+      project_end_date : pr_end_date,
+     };
+
+    let doc = await Project.findOneAndUpdate(filter, update);
+    res.redirect('back');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
 module.exports = router;
+
+    /*const updatedProject = new Project(req.body);
+    var project_name = req.body.project_name;
+    var project_description = req.body.project_description;
+    var project_tasks = req.body.project_tasks;
+    var project_price = req.body.project_price;
+    var project_start_date = req.body.project_start_date;
+    var project_end_date = req.body.project_end_date;
+    
+    var currentProject = await Project.findById(id);
+    currentProject.project_name = project_name;
+    currentProject.project_description = project_description;
+    currentProject.project_tasks = project_tasks;
+    currentProject.project_price = project_price;
+    currentProject.project_start_date = project_start_date;
+    currentProject.project_end_date = project_end_date;
+    await currentProject.save() */
