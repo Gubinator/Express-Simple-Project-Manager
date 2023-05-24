5ef39d463aa441085d08ff03ea590eb8d404b845
Dominik Gubo <gubo.dominik@gmail.com>
2023-05-24 13:51:12 +0200

    Added user model, initialized register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/userModel')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', async(req, res) =>{
+  const locals = {
+    title : "Registration page"
+  }
+  res.render("authentification/register", locals);
+});
+
+router.post('/register', async(req, res) => {
+  const newUser = new User(req.body);
+  newUser.save();
+  res.json(newUser);
+});
+
+
 
 module.exports = router;
